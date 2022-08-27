8818e55bb69cb1b3aafa30b56d6a0404b5d9bcbc
YuriProduction <jurexda@yandex.ru>
2022-08-28 00:00:22 +0500

    Added:1) No border 2) Appeared the new js file(temp)

---
+++ js/temp.js
@@ -0,0 +1,5 @@
+const telephone = document.querySelector('._form3')
+const password = document.querySelector('._form2')
+const login = document.querySelector('._form1')
+
+console.log(telephone.textContent)
