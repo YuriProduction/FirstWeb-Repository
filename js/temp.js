f8150fd7e615c8d837c1d359349cc6e738d3d99d
YuriProduction <jurexda@yandex.ru>
2022-09-01 23:50:48 +0500

    Fixed adapt problem(set font-size=80%), js ready

--- js/temp.js
+++ js/temp.js
@@ -2,4 +2,9 @@ const telephone = document.querySelector('._form3')
 const password = document.querySelector('._form2')
 const login = document.querySelector('._form1')
 
-console.log(telephone.textContent)
+/*let Scroll = (top)=>{
+    const SelectedForm = document.querySelector('.forms');
+    SelectedForm.scrollIntoView(top);
+}
+
+Scroll(true);*/
