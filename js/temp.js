426b8029be216d452bdcb7b79a2edff7e6cb18ca
YuriProduction <jurexda@yandex.ru>
2022-09-02 17:56:08 +0500

    The information about firms is hidden

--- js/temp.js
+++ js/temp.js
@@ -1,10 +1,1 @@
-const telephone = document.querySelector('._form3')
-const password = document.querySelector('._form2')
-const login = document.querySelector('._form1')
 
-/*let Scroll = (top)=>{
-    const SelectedForm = document.querySelector('.forms');
-    SelectedForm.scrollIntoView(top);
-}
-
-Scroll(true);*/
